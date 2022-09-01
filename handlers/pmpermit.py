f766c635383b1e9ea485adff8cd539a493b457f5
Cinarrrddqq <112159362+Cinarrrddqq@users.noreply.github.com>
2022-09-01 09:44:29 +0400

    update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -18,7 +18,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Salam, Mən @LegendMucisBot - un Asistanıyam [Developer](https://t.me/ElnurGenCeLi) 🇦🇿\n",
+                "Salam, Mən @LorddMusicBot - un Asistanıyam [Developer](https://t.me/Rexxuxxnxx) 🇦🇿\n",
             )
             return
  
