68b479c3cdc9a4f4640f231b5613ef3e4ba7a535
Cinarrrddqq <112159362+Cinarrrddqq@users.noreply.github.com>
2022-09-01 09:58:06 +0400

    update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -109,7 +109,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "CreativAsisstan"
+        user.first_name = "CreativAsisstant"
     usar = user
     wew = usar.id
     try:
