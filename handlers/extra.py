4e6fafb1663d213a671672144db454ad77f5ea65
Cinarrrddqq <112159362+Cinarrrddqq@users.noreply.github.com>
2022-09-28 20:59:38 +0400

    update extra.py

--- handlers/extra.py
+++ handlers/extra.py
@@ -2,7 +2,6 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from helpers.filters import command
-from helpers.decorators import sudo_users_only
 from helpers.dbchat import get_served_chats
 
 from config import BOT_USERNAME as bn
